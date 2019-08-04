4a29664237c3fc23c58b4b4ab175c3b163017961
Matthew Gleich <matthewgleich@gmail.com>
2019-08-04 01:05:29 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-
+import os
+import argparse
 
 def main():
     """
-    Runs the main file
-    :return: None
+    Runs the whole program
     """
+    parser = argparse.ArgumentParser(description="Tool for pulling from all of your Git repos")
+    parser.add_argument("-c", "--config", type=str, metavar="", help="Edit the settings for the tool")
+    parser.add_argument("-r", "--run", type=str, metavar="", help="Runs the program")
+    args = parser.parse_args()
+    if args.config is not None:
+        print("Looks like you are gonna configure the system")
+    elif args.run is not None:
+        print("Looks like you wanna run the program")
 
-
-if __name__ == "__main__":
-    main()
-
+main()
