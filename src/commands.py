6a1fd97d8f9d1fc36f0d7372da03144ada8808c1
Matthew Gleich <matthewgleich@gmail.com>
2019-08-04 16:25:14 -0400

    Finish add_command_alias function

--- src/commands.py
+++ src/commands.py
@@ -1,5 +1,7 @@
 import os
 from os import system
+from utils.generic import clear_output
+from time import sleep
 
 def list_dot_files():
     """
@@ -15,11 +17,35 @@ def list_dot_files():
     return dot_files
 
 # Testing
-print(list_dot_files)
+# print(list_dot_files)
 
 
 def add_command_alias():
+    """
+    Adds alias to .bashrc
+    :return: None
+    """
     os.chdir("~")
     dot_files = list_dot_files()
     if ".bashrc" in dot_files:
-        os.system()
+        cwd = os.getcwd() + "/.main.py"
+        system('echo "alias pull=""python"{}"'.format(cwd))
+        clear_output()
+        print("Added command alias to .bashrc")
+    else:
+        clear_output()
+        print("It seems as though there is no .bashrc file located in your root.")
+        make_bashrc = input("Should we make a new one in root?\n")
+        if "y" in make_bashrc:
+            system("touch .bashrc")
+            clear_output()
+            print("Created bashrc")
+        else:
+            clear_output()
+            print("The application can't run without that alias being added to the .bashrc")
+            print("This means that the application will stop in 20 seconds")
+            for i in range(20):
+                sleep(1)
+                print(20 - i)
+
+
