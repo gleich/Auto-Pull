1a38c412b7d9888db9f78c08c20e5373d5186d4b
Matthew Gleich <matthewgleich@gmail.com>
2019-08-07 23:40:27 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,61 @@
 import os
 import argparse
+from os import system
+
+import commands
+from utils import timer
+
+
+def config_warning():
+    """
+    Warns the user how they haven't run the config command yet.
+    """
+    print("It seems as though you haven't ran the config command.\nPlease run that first\nThe program will stop in 10 seconds. Please restart.")
+    timer.countdown(10)
+
 
 def main():
     """
     Runs the whole program
     """
-    parser = argparse.ArgumentParser(description="Tool for pulling from all of your Git repos")
-    parser.add_argument("-c", "--config", type=str, metavar="", help="Edit the settings for the tool")
-    parser.add_argument("-r", "--run", type=str, metavar="", help="Runs the program")
+    parser = argparse.ArgumentParser(
+        description="Tool for pulling from all of your Git repos")
+    parser.add_argument("-c", "--config", type=str, metavar="",
+                        help="Edit the settings for the tool")
+    parser.add_argument("-r", "--run", type=str,
+                        metavar="", help="Runs the program")
     args = parser.parse_args()
     if args.config is not None:
-        print("Looks like you are gonna configure the system")
+        cwd = os.getcwd()
+        commands.add_command_alias()
+        github_folders_location = "~/" + \
+            input("What is the location of your Git folders?\nKeep in mind that the program still works if their are files/folders located in that folder that aren't related to Git.\nPlease make sure that the path is relative to the root.\n~/")
+        try:
+            os.chdir(github_folders_location)
+        except FileNotFoundError:
+            print(
+                "It seems as though that folder doesn't exist.\nThe program will stop in 5 seconds.")
+            timer.countdown(5)
+        os.chdir("~")
+        system("touch auto_pull_config.txt")
+        with open("auto_pull_config.txt", "a") as config_file:
+            github_folder_location_line = "git_folder_location=" + github_folders_location
+            config_file.write(github_folder_location_line)
+        print("Successfully configured the program. You can now run the tool from anywhere using `pull <command>`\n")
     elif args.run is not None:
-        print("Looks like you wanna run the program")
+        os.chdir("~")
+        try:
+            with open("auto_pull_config.txt") as config_file:
+                file_contents = config_file.read()
+            settings_list = {}
+            file_lines = file_contents.split("\n")
+            for line in file_lines:
+                key_value_pair = line.split("=")
+                settings_list[key_value_pair[0]] = key_value_pair[1]
+            if "git_folder_location" not in settings_list.keys():
+                config_warning()
+        except FileNotFoundError:
+            config_warning()
+
 
 main()
